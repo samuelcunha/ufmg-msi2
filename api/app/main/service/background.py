68af59608bf93403831465229eba0eda8cdcbc90
Samuel Cunha <samureira@gmail.com>
2022-07-10 02:39:44 -0300

    Melhorando logs

--- api/app/main/service/background.py
+++ api/app/main/service/background.py
@@ -1,7 +1,7 @@
 from app.main.service.commit_service import find_commits
 from app.main.service.pull_request_service import find_pull_requests
 from app.main.service.repository_service import find_repository_info, set_repositories_to_update, get_pending_repositories, set_repository_processed, set_repository_with_error
-from app.main import scheduler
+from app.main import scheduler, db
 
 
 @scheduler.task('interval', id='do_job_1', seconds=120, misfire_grace_time=900)
@@ -9,22 +9,28 @@ def job1():
     with scheduler.app.app_context():
         try:
             process()
+            db.session.expire_all()
         except Exception as err:
             print("Process error: ", err)
 
 
 def process():
-    print("Processing new repositories")
+    print("Processing pending repositories")
     new_repos = get_pending_repositories()
+    print(len(new_repos), "repositories to process")
     process_new_repositories(new_repos)
-    print("Updating repositories")
+    print("Finished processing pending repositories")
+    
+    print("Scheduling repositories to update")
     set_repositories_to_update()
+    print("Finished scheduling repositories to update")
 
 
 def process_new_repositories(new_repos):
 
     for repo in new_repos:
         try:
+            print("Processing: ", repo.name)
             if find_repository_info(repo):
                 if find_pull_requests(repo):
                     if find_commits(repo):
