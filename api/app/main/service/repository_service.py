9f93a2f2659fde6e745636b88cc993493b0af2c4
Samuel Cunha <samureira@gmail.com>
2022-07-11 01:35:09 -0300

    Incluindo hora de atualização dos repositórios

--- api/app/main/service/repository_service.py
+++ api/app/main/service/repository_service.py
@@ -8,11 +8,12 @@ from app.main.model.repository import Repository
 from typing import Dict, Tuple
 from app.main.service.db import insert
 from app.main import db
-from datetime import date
+from datetime import datetime
 from sqlalchemy import func
 import os
 
 LIMIT = os.getenv('COVERIT_API_SYNC_ITEMS_PER_RUN', 50)
+
 
 def insert_new_repository(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
     repository = Repository.query.filter_by(
@@ -50,8 +51,9 @@ def get_pending_repositories():
 
 
 def set_repositories_to_update():
-    now = date.today()
-    Repository.query.filter(func.date(Repository.updated) < now, Repository.status == RepositoryStatusEnum.SUCCESS).update({'status': RepositoryStatusEnum.PENDING}, synchronize_session='fetch')
+    now = datetime.now()
+    Repository.query.filter(func.date(Repository.updated) < now, Repository.status == RepositoryStatusEnum.SUCCESS).update(
+        {'status': RepositoryStatusEnum.PENDING}, synchronize_session='fetch')
     db.session.commit()
 
 
@@ -64,7 +66,6 @@ def find_repository_info(repo):
         print(error)
         set_repository_with_error(repo, str(error))
         return None
-        
 
 
 def save_repository_info(repo, codecov_info, github_info):
@@ -95,66 +96,33 @@ def save_repository_info(repo, codecov_info, github_info):
 
 def validate_active(repo, codecov_info):
     if not codecov_info or not codecov_info.get('repo') or codecov_info['repo']['active'] is False:
-        update(
-            model=Repository,
-            id=repo.id,
-            data={
-                'status': RepositoryStatusEnum.ERROR,
-                'status_info': "Repositório não encontrado"
-            }
-        )
+        set_repository_with_error(repo, "Repositório não encontrado")
         return False
     return True
 
 
 def validate_coverage(repo, codecov_info):
     if not codecov_info or get_coverage(codecov_info) == 0:
-        update(
-            model=Repository,
-            id=repo.id,
-            data={
-                'status': RepositoryStatusEnum.ERROR,
-                'status_info': "Repositório não possui dados de cobertura"
-            }
-        )
+        set_repository_with_error(
+            repo, "Repositório não possui dados de cobertura")
         return False
     return True
 
 
 def validate_license(repo, github_info):
     if not github_info['license']:
-        update(
-            model=Repository,
-            id=repo.id,
-            data={
-                'status': RepositoryStatusEnum.ERROR,
-                'status_info': "Licença não encontrada"
-            }
-        )
+        set_repository_with_error(repo, "Licença não encontrada")
         return False
     elif not github_info['license'] in LicenseEnum.OPEN_SOURCE:
-        update(
-            model=Repository,
-            id=repo.id,
-            data={
-                'status': RepositoryStatusEnum.ERROR,
-                'status_info': "Repositório não possui uma licença de código aberto"
-            }
-        )
+        set_repository_with_error(
+            repo, "Repositório não possui uma licença de código aberto")
         return False
     return True
 
 
 def validate_language(repo, codecov_info, github_info):
     if not codecov_info.get('repo') or (not codecov_info['repo']['language'] and not github_info['main_language']):
-        update(
-            model=Repository,
-            id=repo.id,
-            data={
-                'status': RepositoryStatusEnum.ERROR,
-                'status_info': "Linguagem principal não encontrada"
-            }
-        )
+        set_repository_with_error(repo, "Linguagem principal não encontrada")
         return False
     return True
 
@@ -164,25 +132,24 @@ def get_coverage(codecov_info):
 
 
 def set_repository_processed(repo):
-
     update(
         model=Repository,
         id=repo.id,
         data={
             'status': RepositoryStatusEnum.SUCCESS,
             'status_info': '',
-            'updated': date.today()
+            'updated': datetime.now()
         }
     )
-    
-def set_repository_with_error(repo, error):
 
+
+def set_repository_with_error(repo, error):
     update(
         model=Repository,
         id=repo.id,
         data={
             'status': RepositoryStatusEnum.ERROR,
             'status_info': error,
-            'updated': date.today()
+            'updated': datetime.now()
         }
     )
